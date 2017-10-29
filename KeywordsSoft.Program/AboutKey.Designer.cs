4fd4297c6d7c9a69ba8c4ec700d74455ff626ee4
e.manushkin <e.manushkin@gmail.com>
2017-10-29 20:58:57 +0200

    redesign of AboutKey form

--- KeywordsSoft.Program/AboutKey.Designer.cs
+++ KeywordsSoft.Program/AboutKey.Designer.cs
@@ -31,29 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutKey));
             this.tabControlAboutKey = new System.Windows.Forms.TabControl();
             this.textsTab = new System.Windows.Forms.TabPage();
-            this.textsTabDataGridView = new System.Windows.Forms.DataGridView();
-            this.imagesTab = new System.Windows.Forms.TabPage();
-            this.imagesTabDataGridView = new System.Windows.Forms.DataGridView();
             this.videosTab = new System.Windows.Forms.TabPage();
-            this.videosTabDataGridView = new System.Windows.Forms.DataGridView();
             this.suggestsTab = new System.Windows.Forms.TabPage();
-            this.suggestsTabDataGridView = new System.Windows.Forms.DataGridView();
             this.snippetsTab = new System.Windows.Forms.TabPage();
-            this.snippetsTabDataGridView = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnBack = new System.Windows.Forms.Button();
             this.labelKeyName = new System.Windows.Forms.Label();
+            this.imagesTab = new System.Windows.Forms.TabPage();
             this.tabControlAboutKey.SuspendLayout();
-            this.textsTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textsTabDataGridView)).BeginInit();
-            this.imagesTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.imagesTabDataGridView)).BeginInit();
-            this.videosTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.videosTabDataGridView)).BeginInit();
-            this.suggestsTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.suggestsTabDataGridView)).BeginInit();
-            this.snippetsTab.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.snippetsTabDataGridView)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -78,7 +63,6 @@
             // textsTab
             // 
             this.textsTab.BackColor = System.Drawing.Color.LightCyan;
-            this.textsTab.Controls.Add(this.textsTabDataGridView);
             this.textsTab.ForeColor = System.Drawing.SystemColors.ControlText;
             this.textsTab.Location = new System.Drawing.Point(4, 25);
             this.textsTab.Name = "textsTab";
@@ -87,61 +71,9 @@
             this.textsTab.TabIndex = 0;
             this.textsTab.Text = "Texts";
             // 
-            // textsTabDataGridView
-            // 
-            this.textsTabDataGridView.AllowUserToAddRows = false;
-            this.textsTabDataGridView.AllowUserToDeleteRows = false;
-            this.textsTabDataGridView.AllowUserToResizeColumns = false;
-            this.textsTabDataGridView.AllowUserToResizeRows = false;
-            this.textsTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.textsTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
-            this.textsTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textsTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.textsTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textsTabDataGridView.Location = new System.Drawing.Point(3, 3);
-            this.textsTabDataGridView.MultiSelect = false;
-            this.textsTabDataGridView.Name = "textsTabDataGridView";
-            this.textsTabDataGridView.ReadOnly = true;
-            this.textsTabDataGridView.RowHeadersVisible = false;
-            this.textsTabDataGridView.Size = new System.Drawing.Size(837, 415);
-            this.textsTabDataGridView.TabIndex = 0;
-            this.textsTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TabDataGridView_CellContentDoubleClick);
-            // 
-            // imagesTab
-            // 
-            this.imagesTab.BackColor = System.Drawing.Color.LightCyan;
-            this.imagesTab.Controls.Add(this.imagesTabDataGridView);
-            this.imagesTab.Location = new System.Drawing.Point(4, 25);
-            this.imagesTab.Name = "imagesTab";
-            this.imagesTab.Padding = new System.Windows.Forms.Padding(3);
-            this.imagesTab.Size = new System.Drawing.Size(843, 421);
-            this.imagesTab.TabIndex = 1;
-            this.imagesTab.Text = "Images";
-            // 
-            // imagesTabDataGridView
-            // 
-            this.imagesTabDataGridView.AllowUserToAddRows = false;
-            this.imagesTabDataGridView.AllowUserToDeleteRows = false;
-            this.imagesTabDataGridView.AllowUserToResizeColumns = false;
-            this.imagesTabDataGridView.AllowUserToResizeRows = false;
-            this.imagesTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.imagesTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
-            this.imagesTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.imagesTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.imagesTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.imagesTabDataGridView.Location = new System.Drawing.Point(3, 3);
-            this.imagesTabDataGridView.MultiSelect = false;
-            this.imagesTabDataGridView.Name = "imagesTabDataGridView";
-            this.imagesTabDataGridView.ReadOnly = true;
-            this.imagesTabDataGridView.RowHeadersVisible = false;
-            this.imagesTabDataGridView.Size = new System.Drawing.Size(837, 415);
-            this.imagesTabDataGridView.TabIndex = 1;
-            this.imagesTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TabDataGridView_CellContentDoubleClick);
-            // 
             // videosTab
             // 
             this.videosTab.BackColor = System.Drawing.Color.LightCyan;
-            this.videosTab.Controls.Add(this.videosTabDataGridView);
             this.videosTab.Location = new System.Drawing.Point(4, 25);
             this.videosTab.Name = "videosTab";
             this.videosTab.Padding = new System.Windows.Forms.Padding(3);
@@ -149,30 +81,9 @@
             this.videosTab.TabIndex = 2;
             this.videosTab.Text = "Videos";
             // 
-            // videosTabDataGridView
-            // 
-            this.videosTabDataGridView.AllowUserToAddRows = false;
-            this.videosTabDataGridView.AllowUserToDeleteRows = false;
-            this.videosTabDataGridView.AllowUserToResizeColumns = false;
-            this.videosTabDataGridView.AllowUserToResizeRows = false;
-            this.videosTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.videosTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
-            this.videosTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.videosTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.videosTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.videosTabDataGridView.Location = new System.Drawing.Point(3, 3);
-            this.videosTabDataGridView.MultiSelect = false;
-            this.videosTabDataGridView.Name = "videosTabDataGridView";
-            this.videosTabDataGridView.ReadOnly = true;
-            this.videosTabDataGridView.RowHeadersVisible = false;
-            this.videosTabDataGridView.Size = new System.Drawing.Size(837, 415);
-            this.videosTabDataGridView.TabIndex = 1;
-            this.videosTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TabDataGridView_CellContentDoubleClick);
-            // 
             // suggestsTab
             // 
             this.suggestsTab.BackColor = System.Drawing.Color.LightCyan;
-            this.suggestsTab.Controls.Add(this.suggestsTabDataGridView);
             this.suggestsTab.Location = new System.Drawing.Point(4, 25);
             this.suggestsTab.Name = "suggestsTab";
             this.suggestsTab.Padding = new System.Windows.Forms.Padding(3);
@@ -180,56 +91,15 @@
             this.suggestsTab.TabIndex = 3;
             this.suggestsTab.Text = "Suggests";
             // 
-            // suggestsTabDataGridView
-            // 
-            this.suggestsTabDataGridView.AllowUserToAddRows = false;
-            this.suggestsTabDataGridView.AllowUserToDeleteRows = false;
-            this.suggestsTabDataGridView.AllowUserToResizeColumns = false;
-            this.suggestsTabDataGridView.AllowUserToResizeRows = false;
-            this.suggestsTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.suggestsTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
-            this.suggestsTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.suggestsTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.suggestsTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.suggestsTabDataGridView.Location = new System.Drawing.Point(3, 3);
-            this.suggestsTabDataGridView.MultiSelect = false;
-            this.suggestsTabDataGridView.Name = "suggestsTabDataGridView";
-            this.suggestsTabDataGridView.ReadOnly = true;
-            this.suggestsTabDataGridView.RowHeadersVisible = false;
-            this.suggestsTabDataGridView.Size = new System.Drawing.Size(837, 415);
-            this.suggestsTabDataGridView.TabIndex = 2;
-            this.suggestsTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TabDataGridView_CellContentDoubleClick);
-            // 
             // snippetsTab
             // 
             this.snippetsTab.BackColor = System.Drawing.Color.LightCyan;
-            this.snippetsTab.Controls.Add(this.snippetsTabDataGridView);
             this.snippetsTab.Location = new System.Drawing.Point(4, 25);
             this.snippetsTab.Name = "snippetsTab";
             this.snippetsTab.Padding = new System.Windows.Forms.Padding(3);
             this.snippetsTab.Size = new System.Drawing.Size(843, 421);
             this.snippetsTab.TabIndex = 4;
             this.snippetsTab.Text = "Snippets";
-            // 
-            // snippetsTabDataGridView
-            // 
-            this.snippetsTabDataGridView.AllowUserToAddRows = false;
-            this.snippetsTabDataGridView.AllowUserToDeleteRows = false;
-            this.snippetsTabDataGridView.AllowUserToResizeColumns = false;
-            this.snippetsTabDataGridView.AllowUserToResizeRows = false;
-            this.snippetsTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.snippetsTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
-            this.snippetsTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.snippetsTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.snippetsTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.snippetsTabDataGridView.Location = new System.Drawing.Point(3, 3);
-            this.snippetsTabDataGridView.MultiSelect = false;
-            this.snippetsTabDataGridView.Name = "snippetsTabDataGridView";
-            this.snippetsTabDataGridView.ReadOnly = true;
-            this.snippetsTabDataGridView.RowHeadersVisible = false;
-            this.snippetsTabDataGridView.Size = new System.Drawing.Size(837, 415);
-            this.snippetsTabDataGridView.TabIndex = 3;
-            this.snippetsTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TabDataGridView_CellContentDoubleClick);
             // 
             // panel1
             // 
@@ -268,6 +138,16 @@
             this.labelKeyName.Size = new System.Drawing.Size(0, 24);
             this.labelKeyName.TabIndex = 0;
             // 
+            // imagesTab
+            // 
+            this.imagesTab.BackColor = System.Drawing.Color.LightCyan;
+            this.imagesTab.Location = new System.Drawing.Point(4, 25);
+            this.imagesTab.Name = "imagesTab";
+            this.imagesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.imagesTab.Size = new System.Drawing.Size(843, 421);
+            this.imagesTab.TabIndex = 1;
+            this.imagesTab.Text = "Images";
+            // 
             // AboutKey
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -286,16 +166,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "AboutKey";
             this.tabControlAboutKey.ResumeLayout(false);
-            this.textsTab.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.textsTabDataGridView)).EndInit();
-            this.imagesTab.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.imagesTabDataGridView)).EndInit();
-            this.videosTab.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.videosTabDataGridView)).EndInit();
-            this.suggestsTab.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.suggestsTabDataGridView)).EndInit();
-            this.snippetsTab.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.snippetsTabDataGridView)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -306,17 +176,12 @@
 
         private System.Windows.Forms.TabControl tabControlAboutKey;
         private System.Windows.Forms.TabPage textsTab;
-        private System.Windows.Forms.TabPage imagesTab;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label labelKeyName;
         private System.Windows.Forms.TabPage videosTab;
         private System.Windows.Forms.TabPage suggestsTab;
         private System.Windows.Forms.TabPage snippetsTab;
         private System.Windows.Forms.Button btnBack;
-        public System.Windows.Forms.DataGridView textsTabDataGridView;
-        public System.Windows.Forms.DataGridView imagesTabDataGridView;
-        public System.Windows.Forms.DataGridView videosTabDataGridView;
-        public System.Windows.Forms.DataGridView suggestsTabDataGridView;
-        public System.Windows.Forms.DataGridView snippetsTabDataGridView;
+        private System.Windows.Forms.TabPage imagesTab;
     }
 }
--- KeywordsSoft.Program/AboutKey.cs
+++ KeywordsSoft.Program/AboutKey.cs
@@ -19,9 +19,13 @@ namespace KeywordsSoft.Program
         string keyId;
         string keyName;
 
+        private System.Windows.Forms.TabControl tabControlParsers;
+        private System.Windows.Forms.DataGridView parserTabDataGridView;
+
         public AboutKey(string _dbName, string _keyId, string _keyName)
         {
             InitializeComponent();
+            InitializeTabsForParsersData();
 
             dbName = _dbName;
             keyId = _keyId;
@@ -48,7 +52,13 @@ namespace KeywordsSoft.Program
             TabLoad((sender as TabControl).SelectedTab.Name);
         }
 
-        private void TabDataGridView_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        private void tabControlParsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var type = tabControlAboutKey.TabPages[tabControlAboutKey.SelectedIndex].Name.Replace("Tab", string.Empty);
+            TabParserLoad((sender as TabControl).SelectedTab.Name, type);
+        }
+
+        private void ParserTabDataGridView_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
             var value = (sender as DataGridView)[e.ColumnIndex, e.RowIndex].Value.ToString();
             MessageBox.Show(value, "Полный текст выбранного элемента", MessageBoxButtons.OK);
@@ -56,102 +66,136 @@ namespace KeywordsSoft.Program
 
         private void TabLoad(string tabName)
         {
-            var dgv = tabControlAboutKey.TabPages[tabName].Controls.Find($"{tabName}DataGridView", true)[0] as DataGridView;
+            tabControlParsers.Parent = tabControlAboutKey.TabPages[tabName];
+            tabControlParsers.SelectedIndex = 0;
+            TabParserLoad(tabControlParsers.TabPages[0].Name, tabName.Replace("Tab", string.Empty));
+        }
 
-            if (dgv.ColumnCount == 0)
+        private void TabParserLoad(string tabName, string parserType)
+        {
+            parserTabDataGridView.ColumnCount = 1;
+            parserTabDataGridView.Rows.Clear();
+            parserTabDataGridView.Refresh();
+            parserTabDataGridView.ColumnHeadersVisible = false;
+
+            parserTabDataGridView.Parent = tabControlParsers.TabPages[tabName];
+
+            var parserName = tabName.Replace("ParserTab", string.Empty);
+
+            var filteredParser = parsers.Where(p => p.type == parserType && p.name == parserName).ToList();
+
+            var filter = $"key_id = {keyId} and parser_id in ({string.Join(", ", filteredParser.Select(x => x.id))})";
+
+            IEnumerable<object> data = null;
+
+            switch (parserType)
             {
-                var type = tabName.Replace("Tab", string.Empty);
-
-                var filteredParser = parsers.Where(p => p.type == type).ToList();
-
-                dgv.ColumnCount = filteredParser.Count;
-                for (int i = 0; i < filteredParser.Count; i++)
-                {
-                    dgv.Columns[i].Name = filteredParser[i].name;
-                }
-
-                var filter = $"key_id = {keyId} and parser_id in ({string.Join(", ", filteredParser.Select(x => x.id))})";
-                var max = 0;
-                var c = 0;
-                List<string> row = new List<string>();
-                List<BasePostfixType> keysData = new List<BasePostfixType>();
-
-                switch (type)
-                {
-                    case Parsers.type_texts:
-                        keysData = new TextsHelper().Select(dbName, filter).Select(x => new BasePostfixType()
-                        {
-                            id = x.id,
-                            key_id = x.key_id,
-                            parser_id = x.parser_id,
-                            text = x.text
-                        }).ToList();
-                        break;
-                    case Parsers.type_images:
-                        keysData = new ImagesHelper().Select(dbName, filter).Select(x => new BasePostfixType()
-                        {
-                            id = x.id,
-                            key_id = x.key_id,
-                            parser_id = x.parser_id,
-                            text = x.text
-                        }).ToList();
-                        break;
-                    case Parsers.type_videos:
-                        keysData = new VideosHelper().Select(dbName, filter).Select(x => new BasePostfixType()
-                        {
-                            id = x.id,
-                            key_id = x.key_id,
-                            parser_id = x.parser_id,
-                            text = x.text
-                        }).ToList();
-                        break;
-                    case Parsers.type_snippets:
-                        keysData = new SnippetsHelper().Select(dbName, filter).Select(x => new BasePostfixType()
-                        {
-                            id = x.id,
-                            key_id = x.key_id,
-                            parser_id = x.parser_id,
-                            text = x.text
-                        }).ToList();
-                        break;
-                    case Parsers.type_suggests:
-                        keysData = new SuggestsHelper().Select(dbName, filter).Select(x => new BasePostfixType()
-                        {
-                            id = x.id,
-                            key_id = x.key_id,
-                            parser_id = x.parser_id,
-                            text = x.text
-                        }).ToList();
-                        break;
-                }
-
-                foreach (var p in filteredParser)
-                {
-                    c = keysData.Count(x => x.parser_id == p.id);
-                    max = c > max ? c : max;
-                }
-
-                for (int i = 0; i < max; i++)
-                {
-                    row.Clear();
-                    for (int j = 0; j < filteredParser.Count; j++)
+                case Parsers.type_texts:
+                    data = new TextsHelper().Select(dbName, filter).Select(x => new string[] { x.text, x.spin, x.url });
+                    if (data != null && data.Any())
                     {
-                        var itemArray = keysData.Where(x => x.parser_id == filteredParser[j].id).ToArray();
-                        if (itemArray != null && itemArray.Length > i)
-                        {
-                            row.Add(itemArray[i].text);
-                        }
-                        else
-                        {
-                            row.Add(null);
-                        }
+                        parserTabDataGridView.ColumnHeadersVisible = true;
+                        parserTabDataGridView.ColumnCount = 3;
+                        parserTabDataGridView.Columns[0].Name = "text";
+                        parserTabDataGridView.Columns[1].Name = "spin";
+                        parserTabDataGridView.Columns[2].Name = "url";
 
+                        foreach (string[] item in data)
+                        {
+                            parserTabDataGridView.Rows.Add(item);
+                        }
                     }
-                    dgv.Rows.Add(row.ToArray());
+                    break;
+                case Parsers.type_images:
+                    DataGridViewRow row1 = new DataGridViewRow();
+                    data = new ImagesHelper().Select(dbName, filter).Select(x => x.text).ToArray();
+                    break;
+                case Parsers.type_videos:
+                    data = new VideosHelper().Select(dbName, filter).Select(x => x.text).ToArray();
+                    break;
+                case Parsers.type_snippets:
+                    data = new SnippetsHelper().Select(dbName, filter).Select(x => x.text).ToArray();
+                    break;
+                case Parsers.type_suggests:
+                    data = new SuggestsHelper().Select(dbName, filter).Select(x => x.text).ToArray();
+                    break;
+            }
+
+            if (parserTabDataGridView.RowCount == 0 && data != null)
+            {
+                foreach (var item in data)
+                {
+                    parserTabDataGridView.Rows.Add(item);
                 }
             }
         }
 
-        
+        private void InitializeTabsForParsersData()
+        {
+            //Initialize parserTabDataGridView
+            this.parserTabDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.parserTabDataGridView)).BeginInit();
+            this.parserTabDataGridView.AllowUserToAddRows = false;
+            this.parserTabDataGridView.AllowUserToDeleteRows = false;
+            this.parserTabDataGridView.AllowUserToResizeColumns = false;
+            this.parserTabDataGridView.AllowUserToResizeRows = false;
+            this.parserTabDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.parserTabDataGridView.BackgroundColor = System.Drawing.Color.LightCyan;
+            this.parserTabDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.parserTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parserTabDataGridView.ColumnHeadersVisible = false;
+            this.parserTabDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.parserTabDataGridView.Location = new System.Drawing.Point(3, 3);
+            this.parserTabDataGridView.MultiSelect = false;
+            this.parserTabDataGridView.Name = "parserTabDataGridView";
+            this.parserTabDataGridView.ReadOnly = true;
+            this.parserTabDataGridView.RowHeadersVisible = false;
+            this.parserTabDataGridView.Size = new System.Drawing.Size(823, 380);
+            this.parserTabDataGridView.TabIndex = 0;
+            this.parserTabDataGridView.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ParserTabDataGridView_CellContentDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.parserTabDataGridView)).EndInit();
+            //End of Initialize parserTabDataGridView
+
+
+            //Initialize tabControlParsers
+            this.tabControlParsers = new System.Windows.Forms.TabControl();
+            this.tabControlParsers.SuspendLayout();
+            this.tabControlParsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControlParsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlParsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControlParsers.Location = new System.Drawing.Point(3, 3);
+            this.tabControlParsers.Name = "tabControlParsers";
+            this.tabControlParsers.SelectedIndex = 0;
+            this.tabControlParsers.Size = new System.Drawing.Size(837, 415);
+            this.tabControlParsers.TabIndex = 0;
+
+            foreach (var parser in parsers.Select(x => x.name).Distinct())
+            {
+                this.tabControlParsers.Controls.Add(CreateNewTab($"{parser}ParserTab", parser));
+            }
+
+            this.tabControlParsers.SelectedIndexChanged += new System.EventHandler(this.tabControlParsers_SelectedIndexChanged);
+            this.tabControlParsers.ResumeLayout(false);
+            //End of Initialize tabControlParsers
+        }
+
+        private TabPage CreateNewTab(string name, string text)
+        {
+            var tab = new System.Windows.Forms.TabPage();
+
+            tab.SuspendLayout();
+            tab.BackColor = System.Drawing.Color.LightCyan;
+            tab.ForeColor = System.Drawing.SystemColors.ControlText;
+            tab.Location = new System.Drawing.Point(4, 25);
+            tab.Name = name;
+            tab.Padding = new System.Windows.Forms.Padding(3);
+            tab.Size = new System.Drawing.Size(829, 386);
+            tab.TabIndex = 0;
+            tab.Text = text;
+            tab.ResumeLayout(false);
+
+            return tab;
+        }
+
     }
 }
